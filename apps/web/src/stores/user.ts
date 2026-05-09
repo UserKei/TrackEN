ea3de71bbd664af35fb9c1c20f760e6818379c10
User_Kei <user_kei@outlook.com>
2026-05-09 13:49:16 +0800

    feat(web): update user store to support word counting and clean up types

--- apps/web/src/stores/user.ts
+++ apps/web/src/stores/user.ts
@@ -1,35 +1,39 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
-import type { WebResultUser, Token, UpdateUser } from "@en/common/user";
-
+import type { WebResultUser, Token, UserUpdate } from "@en/common/user";
 export const useUserStore = defineStore(
   "user",
   () => {
-    const user = ref<WebResultUser | null>(null); // 用户信息
+    //用户信息
+    const user = ref<WebResultUser | null>(null);
+    //设置用户信息
     const setUser = (params: WebResultUser) => {
       user.value = params;
     };
-
-    // 获取 accessToken
+    //导出accessToken
     const getAccessToken = computed(() => user.value?.token.accessToken);
-    // 获取 refreshToken
+    //导出refreshToken
     const getRefreshToken = computed(() => user.value?.token.refreshToken);
-    // 更新 token 信息
+    //更新token
     const updateToken = (newToken: Token) => {
       user.value!.token = newToken;
     };
-    // 更新用户信息
-    const updateUser = (params: UpdateUser) => {
-      user.value!.name = params.name;
-      user.value!.email = params.email;
-      user.value!.address = params.address;
-      user.value!.avatar = params.avatar;
-      user.value!.bio = params.bio;
-      user.value!.isTimingTask = params.isTimingTask;
-      user.value!.timingTaskTime = params.timingTaskTime;
+    //更新用户单词数量
+    const updateUserWordNumber = (wordNumber: number) => {
+      user.value!.wordNumber = wordNumber;
     };
-
-    const getUpdateUserInfo = computed<UpdateUser>(() => {
+    //点击完成保存之后更新用户信息
+    const updateUser = (params: UserUpdate) => {
+      user.value!.name = params.name; //名字
+      user.value!.email = params.email; //邮箱
+      user.value!.address = params.address; //地址
+      user.value!.avatar = params.avatar; //头像
+      user.value!.bio = params.bio; //签名
+      user.value!.isTimingTask = params.isTimingTask; //是否开启定时任务
+      user.value!.timingTaskTime = params.timingTaskTime; //定时任务时间
+    };
+    //在设置界面默认获取的值
+    const getUpdateUserInfo = computed<UserUpdate>(() => {
       return {
         name: user.value!.name,
         email: user.value!.email,
@@ -40,10 +44,11 @@ export const useUserStore = defineStore(
         timingTaskTime: user.value!.timingTaskTime,
       };
     });
-
-    const getUser = computed(() => user.value); // 获取用户信息
+    //获取用户信息
+    const getUser = computed(() => user.value);
+    //退出登录
     const logout = () => {
-      user.value = null; // 退出登录，清除用户信息
+      user.value = null;
     };
     return {
       user,
@@ -55,7 +60,8 @@ export const useUserStore = defineStore(
       updateToken,
       updateUser,
       getUpdateUserInfo,
+      updateUserWordNumber,
     };
   },
   { persist: true },
-); // 开启持久化，数据将保存在 localStorage 中
+); //持久化存储localStorage
