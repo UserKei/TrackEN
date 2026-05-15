7e8057d287fcaeda18859697c70ef52c649f7bd1
User_Kei <user_kei@outlook.com>
2026-05-15 16:19:18 +0800

    feat(web): integrate tracker SDK into web application

--- apps/web/src/App.vue
+++ apps/web/src/App.vue
@@ -10,6 +10,7 @@ import Search from '@/components/Search/index.vue'
 import Login from '@/components/Login/index.vue'
 import { provide, ref, watch } from 'vue'
 import { IS_SHOW_LOGIN } from './components/Login/type';
+import { Tracker } from '@en/tracker'
 
 provide(IS_SHOW_LOGIN, ref(false))
 
@@ -19,10 +20,33 @@ import { useUserStore } from './stores/user';
 const userStore = useUserStore()
 const { connect, disconnect } = useSocket()
 
+
+const tracker = new Tracker({
+  baseUrl: "/api/v1",
+  uv: {
+    api: "/tracker/uv",
+    updateApi: "/tracker/update-uv",
+  },
+  pv: {
+    api: "/tracker/pv",
+  },
+  event: {
+    api: "/tracker/event",
+  },
+  error: {
+    api: "/tracker/error",
+  },
+  performance: {
+    api: "/tracker/performance",
+  },
+});
+
+
 watch(
   () => userStore.user?.id,
   (newVal) => {
     if (newVal) {
+      tracker.setUserId(newVal)
       connect()
     } else {
       disconnect()
